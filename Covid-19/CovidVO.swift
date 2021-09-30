10d8ba59a6dec709a704017e7c745d59aba87628
KES <alice9547@gmail.com>
2021-09-30 16:48:34 +0900

    Codable적용1

--- Covid-19/CovidVO.swift
+++ Covid-19/CovidVO.swift
@@ -7,3 +7,16 @@
 
 import Foundation
 
+struct CovidVO: Decodable{
+    let id: String
+    let area: String
+    let date: String
+    let history: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "CORONA19_ID"
+        case area = "CORONA19_AREA"
+        case date = "CORONA19_DATE"
+        case history = "CORONA19_CONTACT_HISORY"
+    }
+}
--- Covid-19/ListViewController.swift
+++ Covid-19/ListViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 import Alamofire
 
 class ListViewController: UITableViewController {
+    
+    lazy var list: [CovidVO] = {
+        var datalist = [CovidVO]()
+        return datalist
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,28 +22,60 @@ class ListViewController: UITableViewController {
 
     // MARK: - Table view data source
     func callCovidAPI() {
-        let url = "http://openapi.seoul.go.kr:8088/786377506d616c6932335a4b417249/json/Corona19Status/1/5/"
-        let request = AF.request(url)
+        let url = "http://openapi.seoul.go.kr:8088/786377506d616c6932335a4b417249/json/Corona19Status/1/1000"
+        let apiURI: URL! = URL(string: url)
+        let apidata = try! Data(contentsOf:apiURI)
+        let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? ""
+        NSLog("API RESULT = \(log)")
         
-        request.responseJSON { (data) in
-            print(data)
-        }
+        do {
+            let decoder = JSONDecoder()
+            let jsonString = String(data: apidata, encoding: .utf8)
+            let data = jsonString?.data(using: .utf8)
+            
+            
+            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+            
+            let status = apiDictionary["Corona19Status"] as! NSDictionary
+            //let result = status["RESULT"] as! NSArray
+            let covid = status["row"] as! NSArray
+            
+            for row in covid {
+                let r = row as! NSDictionary
+                
+                let cvo = CovidVO()
+                
+                cvo.id = r["CORONA19_ID"] as? String
+                cvo.area = r["CORONA19_AREA"] as? String
+                cvo.date = r["CORONA19_DATE"] as? String
+                cvo.history = r["CORONA19_CONTACT_HISTORY"] as? String
+                
+                self.list.append(cvo)
+            }
+            
+        } catch {}
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return self.list.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let row = self.list[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! CovidCell
 
-        // Configure the cell...
+        cell.id?.text = row.id
+        cell.area?.text = row.area
+        cell.date?.text = row.date
+        cell.history?.text = row.history
 
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        NSLog("선택된 행은 \(indexPath.row) 번째 행이다.")
     }
 
 }
