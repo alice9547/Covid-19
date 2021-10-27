87308e9421cc9648a480f87b6058dbe81d5b9c61
HyeonHoo <easyhoho@naver.com>
2021-10-27 23:35:01 +0900

    CovidVO 변환

--- Covid-19/CovidVO.swift
+++ Covid-19/CovidVO.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct CovidVO: Decodable{
+struct Row: Decodable{
     let id: String
     let area: String
     let date: String
@@ -17,6 +17,26 @@ struct CovidVO: Decodable{
         case id = "CORONA19_ID"
         case area = "CORONA19_AREA"
         case date = "CORONA19_DATE"
-        case history = "CORONA19_CONTACT_HISORY"
+        case history = "CORONA19_CONTACT_HISTORY"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = (try? values.decode(String.self, forKey: .id)) ?? "--id"
+        area = (try? values.decode(String.self, forKey: .area)) ?? "--area"
+        date = (try? values.decode(String.self, forKey: .date)) ?? "--date"
+        history = (try? values.decode(String.self, forKey: .history)) ?? "--history"
+    }
+}
+
+struct Corona19Status: Decodable{
+    let row: [Row]
+}
+
+struct CovidVO: Decodable{
+    let corona19Status: Corona19Status
+    
+    enum CodingKeys: String, CodingKey {
+        case corona19Status = "Corona19Status"
     }
 }
--- Covid-19/ListViewController.swift
+++ Covid-19/ListViewController.swift
@@ -21,40 +21,53 @@ class ListViewController: UITableViewController {
 
     // MARK: - Table view data source
     func callCovidAPI() {
-        let url = "http://openapi.seoul.go.kr:8088/786377506d616c6932335a4b417249/json/Corona19Status/1/1000"
+        let url = "http://openapi.seoul.go.kr:8088/786377506d616c6932335a4b417249/json/Corona19Status/1/10"
         let apiURI: URL! = URL(string: url)
         let apidata = try! Data(contentsOf:apiURI)
         let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? ""
-        NSLog("API RESULT = \(log)")
         
-        do {
-            let decoder = JSONDecoder()
-            let jsonString = String(data: apidata, encoding: .utf8)
-            let data = jsonString?.data(using: .utf8)
-            
-            
-            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
-            
-            let status = apiDictionary["Corona19Status"] as! NSDictionary
-            //let result = status["RESULT"] as! NSArray
-            let covid = status["row"] as! NSArray
-            
-            for row in covid {
-                let r = row as! NSDictionary
+        let decoder = JSONDecoder()
+
+        do{
+            let result = try decoder.decode(CovidVO.self,from: apidata)
+            let covidData = result.corona19Status.row
+
+            for row in covidData{
                 
-                let cvo = CovidVO()
                 
-                cvo.id = r["CORONA19_ID"] as? String
-                cvo.area = r["CORONA19_AREA"] as? String
-                cvo.date = r["CORONA19_DATE"] as? String
-                cvo.history = r["CORONA19_CONTACT_HISTORY"] as? String
-                
-                self.list.append(cvo)
             }
-            
-        } catch {}
+        } catch {
+            print(error)
+        }
         
-    }
+//        do {
+//            let decoder = JSONDecoder()
+//            let jsonString = String(data: apidata, encoding: .utf8)
+//            let data = jsonString?.data(using: .utf8)
+//
+//
+//            let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
+//
+//            let status = apiDictionary["Corona19Status"] as! NSDictionary
+//            //let result = status["RESULT"] as! NSArray
+//            let covid = status["row"] as! NSArray
+//
+//            for row in covid {
+//                let r = row as! NSDictionary
+//
+//                let cvo = CovidVO()
+//
+//                cvo.id = r["CORONA19_ID"] as? String
+//                cvo.area = r["CORONA19_AREA"] as? String
+//                cvo.date = r["CORONA19_DATE"] as? String
+//                cvo.history = r["CORONA19_CONTACT_HISTORY"] as? String
+//
+//                self.list.append(cvo)
+//            }
+//
+//        } catch {}
+//
+}
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.list.count
@@ -65,10 +78,10 @@ class ListViewController: UITableViewController {
         let row = self.list[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! CovidCell
 
-        cell.id?.text = row.id
-        cell.area?.text = row.area
-        cell.date?.text = row.date
-        cell.history?.text = row.history
+//        cell.id?.text = row.id
+//        cell.area?.text = row.area
+//        cell.date?.text = row.date
+//        cell.history?.text = row.history
 
         return cell
     }
