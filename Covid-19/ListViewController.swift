d68217f4f89e8c763aedef1bffed02563bcc6b8b
kes <alice9547@naver.com>
2021-11-02 18:24:08 +0900

    초기화 버튼 생성

--- Covid-19/ListViewController.swift
+++ Covid-19/ListViewController.swift
@@ -16,26 +16,30 @@ class ListViewController: UITableViewController {
     @IBAction func btnPick(_ sender: Any) {
         pickData = []
         txtpick = txtGu.text
-        print("\(txtpick!)버튼 선택")
         
-        for row in covidData{
-            if row.area == txtpick{
-                pickData.append(row)
-                print(pickData)
-            }
-        }
+        pickGu()
+        
+        print("\(txtpick!)버튼 선택")
         self.tableView.reloadData()
     }
-
+    @IBAction func btnReset(_ sender: Any) {
+        pickData = []
+        txtpick = nil
+        
+        pickGu()
+        
+        self.tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.callCovidAPI()
-
+        self.pickGu()
     }
 
     // MARK: - Table view data source
     func callCovidAPI() {
-        let url = "http://openapi.seoul.go.kr:8088/786377506d616c6932335a4b417249/json/Corona19Status/1/10"
+        let url = "http://openapi.seoul.go.kr:8088/786377506d616c6932335a4b417249/json/Corona19Status/1/100"
         let apiURI: URL! = URL(string: url)
 //서버 불안정 할 때 jsonData로 사용
         let apidata = try! Data(contentsOf:apiURI)
@@ -44,23 +48,28 @@ class ListViewController: UITableViewController {
         do{
             let result = try decoder.decode(CovidVO.self,from: apidata)
             covidData = result.corona19Status.row
-            for row in covidData{
-                pickData.append(row)
-            }
+
             print("apidata는 받음")
             
         } catch {
             print(error)
         }
 }
-//    func pickGu() {
-//        for row in covidData{
-//            if row.area == txtpick{
-//                pickData.append(row)
-//            }
-//        }
-//        print("pickGu 호출")
-//    }
+    func pickGu() {
+        if txtpick == nil{
+            for row in covidData{
+                pickData.append(row)
+            }
+        } else {
+            for row in covidData{
+                if row.area == txtpick{
+                    pickData.append(row)
+                }
+            }
+        }
+
+        print("pickGu 호출")
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pickData.count
