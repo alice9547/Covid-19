f86525213b76c1f4b5115bb524a4397cc8898b39
KES <alice9547@naver.com>
2021-11-02 15:19:25 +0900

    검색 후 찾기

--- Covid-19/ListViewController.swift
+++ Covid-19/ListViewController.swift
@@ -45,13 +45,13 @@ class ListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 30
+        return nowonData.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let row = self.nowonData[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! CovidNowonCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! CovidCell
 
         cell.id?.text = row.id
         cell.area?.text = row.area
